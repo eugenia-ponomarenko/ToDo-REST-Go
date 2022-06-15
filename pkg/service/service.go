0d915409b3c1c6e136288655b4b7c080e60481fb
eugenia-ponomarenko <zhenka.ponoarenko@gmail.com>
2022-06-15 09:59:51 +0000

    Added Sign Up logic

--- pkg/service/service.go
+++ pkg/service/service.go
@@ -1,9 +1,12 @@
 package service
 
-import "github.com/eugenia-ponomarenko/ToDo-REST-Go/pkg/repository"
+import (
+	"github.com/eugenia-ponomarenko/ToDo-REST-Go/pkg/repository"
+	"github.com/eugenia-ponomarenko/ToDo-REST-Go"
+)
 
 type Authorization interface {
-
+	CreateUser(user todo.User) (int, error)
 }
 
 type TodoList interface {
@@ -21,5 +24,7 @@ type Service struct {
 }
 
 func NewService(repos *repository.Repository) *Service {
-	return &Service{}
+	return &Service{
+		Authorization: NewAuthService(repos.Authorization),
+	}
 }
