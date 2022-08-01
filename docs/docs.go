6f6c0cdda3d82c21325cf83dcbfb1a995a4cb647
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-08-01 13:29:05 +0300

    Update docs.go

--- docs/docs.go
+++ docs/docs.go
@@ -401,7 +401,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "193.122.54.72:8000",
+	Host:             "localhost:8000",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Todo App API",
