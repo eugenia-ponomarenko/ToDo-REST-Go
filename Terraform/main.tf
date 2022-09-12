0e9fff7191b261673a01b631e335ebf8bebd18ac
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-09-12 17:29:08 +0300

    Update main.tf

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -95,7 +95,7 @@ resource "aws_security_group" "ubuntuSecurityGroup" {
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
