cb7fd438eac76a833711ec3db84317162bde46f9
Yevheniia Ponomarenko <yponoma@MacBook-Pro-Yevheniia.local>
2022-09-13 14:38:43 +0300

    Change egress protocol from tcp to -1 due to the impossibility of installing tools through Ansible

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -95,7 +95,7 @@ resource "aws_security_group" "ubuntuSecurityGroup" {
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "tcp"
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
