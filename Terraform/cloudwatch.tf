ae3fed45d0a416f14e7f374777a28318c2ff0c52
Yevheniia Ponomarenko <yponoma@MacBook-Pro-Yevheniia.local>
2022-10-07 13:28:32 +0300

    Add cloudwatch metric alarm

--- Terraform/cloudwatch.tf
+++ Terraform/cloudwatch.tf
@@ -117,3 +117,20 @@ EOF
       aws_instance.u_web_server
     ]
 }
+
+resource "aws_cloudwatch_metric_alarm" "ec2_cpu" {
+    alarm_name                = "ec2-cpu-utilization"
+    comparison_operator       = "GreaterThanOrEqualToThreshold"
+    evaluation_periods        = "2"
+    metric_name               = "CPUUtilization"
+    namespace                 = "AWS/EC2"
+    period                    = "120" #seconds
+    statistic                 = "Average"
+    threshold                 = "70"
+    alarm_description         = "ToDo-App EC2 Instance CPU Utilization Alarm"
+    insufficient_data_actions = []
+
+    dimensions = {
+        InstanceId = aws_instance.u_web_server.id
+        }
+}
