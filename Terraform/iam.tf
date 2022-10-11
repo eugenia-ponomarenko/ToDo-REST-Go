9bbdd6366b8fbfa2f6474e00597fc675ab553f3b
Yevheniia Ponomarenko <yponoma@MacBook-Pro-Yevheniia.local>
2022-10-11 11:34:58 +0300

    Update aws_iam_role by using jsonencode instead of EOF

--- Terraform/iam.tf
+++ Terraform/iam.tf
@@ -1,20 +1,19 @@
 resource "aws_iam_role" "ToDo_accessToRDS" {
   name = "ToDo-Access-RDS"
-  assume_role_policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": "sts:AssumeRole",
-      "Principal": {
-        "Service": "ec2.amazonaws.com"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
       },
-      "Effect": "Allow",
-      "Sid": ""
-    }
-  ]
-}
-EOF
+    ]
+  })
 }
 
 resource "aws_iam_policy" "ec2_to_rds" {
