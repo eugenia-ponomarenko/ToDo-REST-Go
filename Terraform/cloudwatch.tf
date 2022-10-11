7898b11f3f011ae29300ec178e2ceb21cecf5841
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-10-11 12:51:15 +0300

    Update cloudwatch.tf

--- Terraform/cloudwatch.tf
+++ Terraform/cloudwatch.tf
@@ -177,7 +177,7 @@ resource "aws_cloudwatch_dashboard" "main" {
 EOF
 
     depends_on = [
-      aws_instance.u_web_server
+      aws_instance.u_web_server,
       aws_db_instance.ToDo_RDS_instance
     ]
 }
