cbe06dfc16e01744e3aef5b162f4c8ff1de1d224
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-10-11 17:07:03 +0300

    Update sg.tf

--- Terraform/sg.tf
+++ Terraform/sg.tf
@@ -37,7 +37,8 @@ resource "aws_security_group" "RDS_SecurityGroup" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = ["${aws_security_group.EC2_SecurityGroup.id}", "${var.jenkins_public_ip}/32"]
+    cidr_blocks = ["${var.jenkins_public_ip}/32"]
+    security_groups = ["${aws_security_group.EC2_SecurityGroup.id}"]
   }
 
   egress {
