760b68a3d49dc6965c9055cac091a0757e4bbb2f
Yevheniia Ponomarenko <yponoma@MacBook-Pro-Yevheniia.local>
2022-10-11 12:55:34 +0300

    Limit traffic to ec2 and rds instances

--- Terraform/sg.tf
+++ Terraform/sg.tf
@@ -16,7 +16,7 @@ resource "aws_security_group" "EC2_SecurityGroup" {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${var.jenkins_public_ip}/32"]
   }
 
   egress {
@@ -37,7 +37,7 @@ resource "aws_security_group" "RDS_SecurityGroup" {
     from_port   = 5432
     to_port     = 5432
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["${aws_instance.u_web_server.public_ip}/32", "${var.jenkins_public_ip}/32"]
   }
 
   egress {
