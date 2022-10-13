836ac3b471687dc6d4760ac4b945e86b97f48c36
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-10-13 12:44:58 +0300

    Update outputs.tf

--- Terraform/lb_vpc_rds/outputs.tf
+++ Terraform/lb_vpc_rds/outputs.tf
@@ -16,13 +16,13 @@ output "ecs_sg_id" {
 }
 
 output "public_subnet_0" {
-  value = aws_subnet.public_subnet.0.id
+  value = aws_subnet.public.0.id
 }
 
 output "public_subnet_1" {
-  value = aws_subnet.public_subnet.1.id
+  value = aws_subnet.public.1.id
 }
 
 output "public_subnet_2" {
-  value = aws_subnet.public_subnet.2.id
+  value = aws_subnet.public.2.id
 }
