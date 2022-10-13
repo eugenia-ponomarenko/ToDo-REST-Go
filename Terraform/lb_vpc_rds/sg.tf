83a3328b9db55672539f1fa0d8ca466e4b481d68
eugenia-ponomarenko <71873090+eugenia-ponomarenko@users.noreply.github.com>
2022-10-13 12:46:53 +0300

    Update sg.tf

--- Terraform/lb_vpc_rds/sg.tf
+++ Terraform/lb_vpc_rds/sg.tf
@@ -1,3 +1,22 @@
+resource "aws_security_group" "lb" {
+  name        = "todo-app-alb-security-group"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
 resource "aws_security_group" "ECS_SecurityGroup" {
   name        = "ToDo App SecurityGroup"
   description = "ToDo_APP. SecurityGroup for EC2 instance"
